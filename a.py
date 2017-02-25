96be95e30a9a44bbb40aef11d753f50c663a0c73
张小奕 <zxyqwe_2004@126.com>
2017-02-25 17:12:22 +0800

    new

--- a.py
+++ a.py
@@ -1,36 +1,69 @@
-#!/bin/python
+#!/usr/bin/python
 
-import os
-import time, hashlib
+import re, hashlib
+import glob
 from PIL import Image
 import oss2
-from config import ACCESS, KEY, BNAME
-
-auth = oss2.Auth(ACCESS, KEY)
-endpoint = 'http://oss-cn-beijing.aliyuncs.com'
-bucket = oss2.Bucket(auth, endpoint, BNAME)
-
-for m in os.walk('video'):
-    b = m[2]
-for m in os.walk('pic'):
-    a = m[2]
+from config import ACCESS, KEY, BNAME, ENDPOINT
 
 
-def resize(name, r=100.0):
-    im = Image.open('pic/' + name)
+def main():
+    auth = oss2.Auth(ACCESS, KEY)
+    bucket = oss2.Bucket(auth, ENDPOINT, BNAME)
+
+    vlist = list(glob.glob(r'./video/*.mp4'))
+    plist = list(glob.glob(r'./pic/*.jpg'))
+    vlist.reverse()
+    plist.reverse()
+
+    print vlist, plist
+
+    if len(plist) > 0:
+        pic(plist, bucket)
+
+    if len(vlist) > 0:
+        video(vlist, bucket)
+    replace(bucket)
+
+
+def replace(bucket):
+    plist = list(glob.glob(r'./js/p*.js'))
+    pattern = re.compile(r'\d+-[a-z0-9A-Z]{64}')
+    for m in plist:
+        print m
+        content = open(m).read()
+        flist = re.findall(pattern, content)
+        for f in flist:
+            num = f.split('-')[0]
+            num = int(num)
+            if num > 200000:
+                bucket.get_object_to_file(f, r'./pic/tmp.jpg')
+                cnew = resize(r'./pic/tmp.jpg', bucket, 1000)
+                content = content.replace(f, cnew)
+                open(m, 'w').write(content)
+                bucket.delete_object(f)
+
+
+def resize(name, bucket, r=100.0):
+    im = Image.open(name)
     lam = int(round(min(im.width / r, im.height / r)))
+    if lam == 0:
+        lam = 1
     im = im.resize((im.width / lam, im.height / lam), Image.BILINEAR)
-    im.save('pic1/' + name)
-    with open('pic1/' + name, 'rb') as f:
-        return f.read()
+    im.save('./pic1/' + name)
+    with open('./pic1/' + name, 'rb') as f:
+        content = f.read()
+    return up(content, bucket)
 
 
 def s(a):
-    s = str(len(a)) + '-' + hashlib.sha256(a).hexdigest()
-    return str(s)
+    s256 = str(len(a)) + '-' + hashlib.sha256(a).hexdigest()
+    return str(s256)
 
 
-def up(name, headers={'Content-Type': 'image/jpeg', 'Cache-Control': 'public'}):
+def up(name, bucket, headers=None):
+    if headers is None:
+        headers = {'Content-Type': 'image/jpeg', 'Cache-Control': 'public'}
     sha = s(name)
     try:
         bucket.object_exists(sha)
@@ -39,37 +72,37 @@ def up(name, headers={'Content-Type': 'image/jpeg', 'Cache-Control': 'public'}):
     return sha
 
 
-def pic(r=100.0):
-    a.reverse()
+def pic(a, bucket, r=100.0):
     for m in a:
-        with open('pic/' + m, 'rb') as f:
-            all = f.read()
-        all = up(all)
-        m = resize(m, r)
-        m = up(m)
-        print '{ori:"' + all + '",thu:"' + m + '"},'
+        content = resize(m, bucket, 1000)
+        m = resize(m, bucket, r)
+        print '{ori:"' + content + '",thu:"' + m + '"},'
 
 
-def video():
-    b.reverse()
+def video(b, bucket):
     for m in b:
-        with open('video/' + m, 'rb') as f:
-            all = f.read()
-        all = up(all, {'Content-Type': 'video/mp4', 'Cache-Control': 'public'})
-        print "{ href: '" + all + "', time: '" + m + "' },"
+        with open(m, 'rb') as f:
+            content = f.read()
+        headers = {'Content-Type': 'video/mp4', 'Cache-Control': 'public'}
+        content = up(content, bucket, headers)
+        print "{ href: '" + content + "', time: '" + m + "' },"
 
 
-def index():
+def index(bucket):
     bucket.put_object('index.html', open('html/index.html').read(),
                       headers={'Cache-Control': 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0'})
     bucket.batch_delete_objects(['test.html'])  # ,'video.html'
 
 
-def test():
+def test(bucket):
     bucket.put_object('test.html', open('index.html').read(),
                       headers={'Cache-Control': 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0'})
 
 
-def js():
-    for m in []:  # 'v1611.js','v1612.js','v1610.js','p1610.js','p1611.js','p1612.js','v1607.js','v1608.js','v1609.js','p1607.js','p1608.js','p1609.js'
+def js(bucket, plist):
+    for m in plist:
         bucket.put_object(m, open(m).read())
+
+
+if __name__ == '__main__':
+    main()
